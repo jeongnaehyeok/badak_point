51de380da64c1eeba2fee4a35cd1179cd51fe53e
NaeHyeok Jeong <cnh0301@gmail.com>
2022-05-21 17:12:35 +0900

    fix: change low and high typo

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ send_email = os.environ.get('SEND_EMAIL')
 recv_email = os.environ.get('RECV_EMAIL').split(',')
 password = os.environ.get('PASSWORD')
 
-us_high = week_52.get_low_count('usa')
-us_low = week_52.get_high_count('usa')
+us_low = week_52.get_low_count('usa')
+us_high = week_52.get_high_count('usa')
 
-kr_high = week_52.get_low_count('south-korea')
-kr_low = week_52.get_high_count('south-korea')
+kr_low = week_52.get_low_count('south-korea')
+kr_high = week_52.get_high_count('south-korea')
 
 text = mail_service.generate_hyeonho_point_text(us_high, us_low, kr_high, kr_low)
 mail_service.send(text, send_email, recv_email, password)
