29b39126bb9f01331c2481224f4ba092d51a91bd
naehyeok.jeong <cnh0301@gmail.com>
2022-09-29 17:35:43 +0900

    feature:notion DB에 자동으로 추가하는 기능

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { sendPointMail } = require("./utils/sendPointMail");
+const { updateBadakPointDB } = require("./utils/notion");
 
 sendPointMail().catch((e) => {
+  console.log(e);
+});
+
+updateBadakPointDB().catch((e) => {
   console.log(e);
 });
---
+++ utils/notion.js
@@ -0,0 +1,54 @@
+const { Client } = require("@notionhq/client");
+const { formatInTimeZone } = require("date-fns-tz");
+const { getPoint } = require("./getPoint");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const countrys = {
+  usa: process.env.NOTION_US_DATABASE_ID,
+  "south-korea": process.env.NOTION_KR_DATABASE_ID,
+};
+
+const notion = new Client({ auth: process.env.NOTION_USER_KEY });
+
+const addBadakPointDB = async (country) => {
+  const date = formatInTimeZone(new Date(), "Asia/Seoul", "yyyy-MM-dd");
+  const { point, high, low } = await getPoint(country);
+
+  return notion.pages
+    .create({
+      parent: { database_id: countrys[country] },
+      properties: {
+        날짜: {
+          title: [
+            {
+              type: "text",
+              text: {
+                content: date,
+              },
+            },
+          ],
+        },
+        "바닥 지수": {
+          type: "number",
+          number: point,
+        },
+        최고가: {
+          type: "number",
+          number: high,
+        },
+        최저가: {
+          type: "number",
+          number: low,
+        },
+      },
+    })
+    .catch((e) => console.log(e));
+};
+
+module.exports.updateBadakPointDB = async () => {
+  await addBadakPointDB("usa");
+  await addBadakPointDB("south-korea");
+};
